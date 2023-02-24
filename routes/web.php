71e0ea873a6b7c07a8cc96bb5d7e2631051a119d
md-arif-islam <arifislamarif344@gmail.com>
2023-02-24 14:46:53 +0600

    Routing

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
+
+Route::get( '/hello', function () {
+    return "Hello Uniserve!";
+} );
+
+Route::get( '/hello/{params}', function ( $params ) {
+    return "Hello {$params}";
+} );

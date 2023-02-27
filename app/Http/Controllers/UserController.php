dd6b9f5ddd39da379e220d28af0649d2d9f47aba
md-arif-islam <arifislamarif344@gmail.com>
2023-02-27 09:57:46 +0600

    HTTP API fetch

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller {
-    public function loginForm( Request $request ) {
-        return $request->input();
+
+    public function index() {
+        $data = Http::get( "https://reqres.in/api/users?page=1" );
+        return view( "users", ["data" => $data['data']] );
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,4 @@ Route::get( '/', function () {
     return view( 'welcome' );
 } );
 
-Route::view( '/about', 'about' )->middleware( "age_protected" );
-Route::view( '/contact', 'contact' );
-
-Route::view( '/noaccess', 'noaccess' );
+Route::get( 'users', [UserController::class, "index"] );
